d8572151e1f87cd250f5f2d34b894392b99ac4ec
Patrick Spek <p.spek@tyil.nl>
2017-10-13 01:19:52 +0200

    Put test in a seperate script

---
+++ .travis.d/test.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env sh
+
+main()
+{
+	readonly target=$1
+
+	# Deduce the image to use
+	case "$target" in
+		archlinux) image=base/archlinux         ;;
+		amazon)    image=amazonlinux            ;;
+		debian)    image=bitnami/minideb-extras ;;
+		funtoo)    image=mastersrp/funtoo       ;;
+		*)         image=$1
+	esac
+
+	# Create the docker instance
+	docker run -t -d --name "$target" "$image" bash
+
+	# Run bootstrap test
+	sparrowdo --docker="$target" --no_sudo --bootstrap --task_run=bash@command=uptime
+}
+
+main "$@"
