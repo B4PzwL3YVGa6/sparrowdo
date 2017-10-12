6e06aeaabce683d5f62cee7061cd95afe74ceef2
Patrick Spek <p.spek@tyil.nl>
2017-10-13 01:31:52 +0200

    Change ~ to $HOME

--- .travis.d/test.sh
+++ .travis.d/test.sh
@@ -5,7 +5,7 @@ main()
 	readonly target=$1
 
 	# Extend the PATH
-	export PATH=/opt/rakudo/bin/:~/.perl6/bin:$PATH
+	export PATH=/opt/rakudo/bin/:$HOME/.perl6/bin:$PATH
 
 	# Deduce the image to use
 	case "$target" in
