2672e8f617351d62c0f1bf8ae3789248f7dbe222
Alexey Melezhik <melezhik@gmail.com>
2018-05-30 20:31:08 +0000

    try openshift centos

--- .travis.d/tests/api.sh
+++ .travis.d/tests/api.sh
@@ -4,7 +4,7 @@ main()
 {
 
 	# Create the docker instance
-  docker run --privileged --name centos --entrypoint  init -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d centos
+  docker run --privileged --name centos --entrypoint  init -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d openshift/base-centos7
 
   cd ../ && strun --root examples/ --param flavor=travis
 
