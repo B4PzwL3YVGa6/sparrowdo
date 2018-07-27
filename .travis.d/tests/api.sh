bd39e50a5c020d7d757abb9b7f84b668774689c5
Alexey Melezhik <melezhik@gmail.com>
2018-07-27 21:11:22 +0000

    use ubuntu instead of centos to run api tests

--- .travis.d/tests/api.sh
+++ .travis.d/tests/api.sh
@@ -4,7 +4,7 @@ main()
 {
 
 	# Create the docker instance
-  docker run --privileged --name centos --entrypoint  init -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d openshift/base-centos7
+  docker run --privileged --name ubuntu --entrypoint  init -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d ubuntu
 
   cd ../ && strun --root examples/ --param flavor=travis
 
