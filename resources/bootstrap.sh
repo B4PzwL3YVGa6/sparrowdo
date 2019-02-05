c644888842d759f704bdc7ddb5106acbb4dd8bf7
Alexey Melezhik <melezhik@gmail.com>
2019-02-04 20:56:52 -0600

    adjust some setting for package managers varios so

--- resources/bootstrap.sh
+++ resources/bootstrap.sh
@@ -7,7 +7,7 @@ case "$OS" in
     yum -q -y install make curl perl-JSON bash redhat-lsb-core git
     rm -rf /etc/yum.repos.d/rakudo-pkg.repo
     echo -e "[rakudo-pkg]\nname=rakudo-pkg\nbaseurl=https://dl.bintray.com/nxadm/rakudo-pkg-rpms/`lsb_release -is`/`lsb_release -rs| cut -d. -f1`/x86_64\ngpgcheck=0\nenabled=1" | tee -a /etc/yum.repos.d/rakudo-pkg.repo
-    yum install rakudo-pkg
+    yum -q -y install rakudo-pkg
   ;;
   arch|archlinux)
     pacman -Syy
@@ -16,17 +16,17 @@ case "$OS" in
   debian|ubuntu)
     DEBIAN_FRONTEND=noninteractive
     apt-get update
-    apt-get install -y -qq build-essential curl libjson-perl bash git
+    apt-get install -y -qq build-essential curl libjson-perl bash git lsb-release
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 379CE192D401AB61
     rm -rf /etc/apt/sources.list.d/rakudo-pkg.list
     echo "deb https://dl.bintray.com/nxadm/rakudo-pkg-debs `lsb_release -cs` main" | tee -a /etc/apt/sources.list.d/rakudo-pkg.list
-    apt-get update && apt-get install rakudo-pkg
+    apt-get update -qq && apt-get install -y -qq rakudo-pkg
   ;;
   fedora)
-    dnf -y install curl perl-JSON bash redhat-lsb-core git
+    dnf -yq install curl perl-JSON bash redhat-lsb-core git
     rm -rf /etc/yum.repos.d/rakudo-pkg.repo
     echo -e "[rakudo-pkg]\nname=rakudo-pkg\nbaseurl=https://dl.bintray.com/nxadm/rakudo-pkg-rpms/`lsb_release -is`/`lsb_release -rs| cut -d. -f1`/x86_64\ngpgcheck=0\nenabled=1" | tee -a /etc/yum.repos.d/rakudo-pkg.repo
-    dnf install rakudo-pkg
+    dnf -yq install rakudo-pkg
   ;;
   *)
     printf "Your OS (%s) is not supported\n" "$OS"
