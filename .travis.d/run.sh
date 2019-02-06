04b11c7aecccca3a7f3fa6b9730f6c7cb3bb8aed
Alexey Melezhik <melezhik@gmail.com>
2019-02-06 17:02:07 +0000

    fix mv error

--- .travis.d/run.sh
+++ .travis.d/run.sh
@@ -4,7 +4,6 @@ distro=$1;
 
 PATH=/usr/local/bin:/opt/rakudo-pkg/bin/:/home/travis/.perl6/bin:$PATH
 
-mv sparrowfile.off sparrowfile
-
 sparrowdo --docker=$distro --verbose --sparrowfile=examples/hello-world.pl6
+sparrowdo --docker=$distro --verbose --sparrowfile=examples/asserts.pl6
 
