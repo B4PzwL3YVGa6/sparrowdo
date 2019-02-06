f0f58bafb328597078665df5b800f57825cb7785
Alexey Melezhik <melezhik@gmail.com>
2019-02-06 21:53:15 +0000

    add assert http ok test

--- .travis.d/run.sh
+++ .travis.d/run.sh
@@ -8,6 +8,7 @@ if test $distro =  "ubuntu"; then
 
     sparrowdo --docker=$distro --verbose --sparrowfile=examples/hello-world.pl6
     sparrowdo --docker=$distro --verbose --sparrowfile=examples/asserts/sparrowfile
+    sparrowdo --docker=$distro --verbose --sparrowfile=examples/asserts-http-ok/sparrowfile
 
 else
 
