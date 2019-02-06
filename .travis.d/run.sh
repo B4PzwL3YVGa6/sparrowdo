4d45a29aa17f77a3d5c921c997cbfbbcd8ec40f5
Alexey Melezhik <melezhik@gmail.com>
2019-02-06 17:44:19 +0000

    run full tests on ubuntu only

--- .travis.d/run.sh
+++ .travis.d/run.sh
@@ -4,6 +4,14 @@ distro=$1;
 
 PATH=/usr/local/bin:/opt/rakudo-pkg/bin/:/home/travis/.perl6/bin:$PATH
 
-sparrowdo --docker=$distro --verbose --sparrowfile=examples/hello-world.pl6
-sparrowdo --docker=$distro --verbose --sparrowfile=examples/asserts.pl6
+case "$distro" in
+  ubuntu)
+    sparrowdo --docker=$distro --verbose --sparrowfile=examples/hello-world.pl6
+    sparrowdo --docker=$distro --verbose --sparrowfile=examples/asserts/sparrowfile
+  ;;
+    sparrowdo --docker=$distro --verbose --sparrowfile=examples/hello-world.pl6
+  *)
+
+esac
+
 
--- examples/asserts.pl6
+++
@@ -1,2 +0,0 @@
-http-ok "https://sparrowhub.org";
-
