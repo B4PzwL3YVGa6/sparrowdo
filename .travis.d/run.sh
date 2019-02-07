18a3a34b6536fae783d9c3f3314109961e091423
Alexey Melezhik <melezhik@gmail.com>
2019-02-07 15:26:42 +0000

    run full tests on every distro!

--- .travis.d/run.sh
+++ .travis.d/run.sh
@@ -1,20 +1,12 @@
 set -e
 
-distro=$1;
+distro=$1
+
+sparrowfile=$2
 
 PATH=/usr/local/bin:/opt/rakudo-pkg/bin/:~/.perl6/bin:$PATH
 
-if test $distro =  "ubuntu"; then
+sparrowdo --docker=$distro --verbose --sparrowfile=$sparrowfile
 
-    sparrowdo --docker=$distro --verbose --sparrowfile=examples/hello-world.pl6
-    sparrowdo --docker=$distro --verbose --sparrowfile=examples/asserts/sparrowfile
-    sparrowdo --docker=$distro --verbose --sparrowfile=examples/asserts-http-ok/sparrowfile
-    sparrowdo --docker=$distro --verbose --sparrowfile=examples/bash/sparrowfile
-
-else
-
-    sparrowdo --docker=$distro --verbose --sparrowfile=examples/hello-world.pl6
-
-fi
 
 
